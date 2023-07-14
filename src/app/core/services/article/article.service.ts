84cd3aac829f1e4b49c83c4a429137a12fa5eb9c
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-07-14 20:38:11 +0800

    Setting up image uploading w imagekit in form

--- src/app/core/services/article/article.service.ts
+++ src/app/core/services/article/article.service.ts
@@ -3,6 +3,7 @@ import { ServerService } from '../server/server.service';
 import { Subject } from 'rxjs';
 import { StateService } from '../state/state.service';
 import { FormGroup } from '@angular/forms';
+import { FormService } from '../form/form.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,7 @@ export class ArticleService {
   api: string = 'api/v1/articles'
   constructor(
     private service: ServerService,
+    private formService: FormService,
     private stateService: StateService
   ) {
     this.model = new Subject();
@@ -28,12 +30,19 @@ export class ArticleService {
   }
 
   async report(form: FormGroup) {
-    let formData = new FormData();
-    Object.keys(form.value).forEach((key) => {
-      if ((form.value as any)[key])
-        formData.append(key, (form.value as any)[key]);
-    });
-    const res = await this.service.post(this.api, form.value); 
+    // let formData = new FormData();
+    // Object.keys(form.value).forEach((key) => {
+    //   if ((form.value as any)[key]) {
+    //     if(key == 'images') {
+    //       formData.append(key, (form.value as any)[key].map((image: any) => image.file));
+    //     } else {
+    //       formData.append(key, (form.value as any)[key]);
+    //     }
+    //   }
+         
+       
+    // });
+    const res = await Promise.all([this.service.post(this.api, form.value), this.formService.uploadToImageKit(form.value['images'])]); 
     console.log(res)
   }
 }
--- src/app/core/services/form/form.service.ts
+++ src/app/core/services/form/form.service.ts
@@ -24,10 +24,26 @@ export class FormService {
 
   async uploadToImageKit(images: any) {
     const imagekit = new ImageKit({
-      publicKey: "your_public_key",
-      urlEndpoint: "your_url_endpoint", //  
+      publicKey: environment.imagekit_public_key,
+      urlEndpoint: environment.imagekit_private_key, //  
       authenticationEndpoint: `${environment.endpoint_mapnews_backend_api}/signature`
   });
+
+    for(let image of images) {
+      imagekit.upload({
+        file: image.file,
+        fileName: image.file.name || "sample-file.jpg", 
+        folder: "Articles"
+    }, function (err: any, result: any) {
+        if (err) {
+            alert("Error in file upload. Check console logs for error response");
+            console.log(err);
+        } else {
+            alert("File uploaded. Check console logs for success response");
+            console.log(result);
+        }
+    })
+    }
   }
 
 }
