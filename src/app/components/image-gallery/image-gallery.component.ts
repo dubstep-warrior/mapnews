8061f2710338236b4ff54eb2544a7aee42b54666
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-07-15 19:15:13 +0800

    Add image gallery to article details

---
+++ src/app/components/image-gallery/image-gallery.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image-gallery',
+  templateUrl: './image-gallery.component.html',
+  styleUrls: ['./image-gallery.component.scss']
+})
+export class ImageGalleryComponent {
+  @Input() images: Array<any>
+  position: "bottom" | "top" | "left" | "right" = 'bottom';
+
+  positionOptions = [
+    {
+        label: 'Bottom',
+        value: 'bottom'
+    },
+    {
+        label: 'Top',
+        value: 'top'
+    },
+    {
+        label: 'Left',
+        value: 'left'
+    },
+    {
+        label: 'Right',
+        value: 'right'
+    }
+];
+
+responsiveOptions: any[] = [
+    {
+        breakpoint: '1024px',
+        numVisible: 5
+    },
+    {
+        breakpoint: '768px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '560px',
+        numVisible: 1
+    }
+];
+}
