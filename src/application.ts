c0f5d300600dbb23bfd64d83c9b0433d976814e1
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-07-18 04:03:42 +0800

    Set CORS headers

--- src/application.ts
+++ src/application.ts
@@ -29,7 +29,7 @@ class Application {
     // this._instance.use(cors(corsOptions));
 
     const allowCrossDomain = (req: any, res: any, next: any) => {
-      res.header(`Access-Control-Allow-Origin`, `http://localhost:4200`);
+      res.header(`Access-Control-Allow-Origin`, `*`);
       res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
       res.header(`Access-Control-Allow-Headers`, `Content-Type`);
       next();
