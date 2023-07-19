e2679dcecb4d6352e10a67e25b15a775e6f93a03
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-07-19 20:12:27 +0800

    Update navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
 import { StateService } from 'src/app/core/services/state/state.service';
+import { slideInFromLeft, slideInFromRight } from 'src/app/core/utilities/animations';
 
 @Component({
   selector: 'app-navbar',
+  animations: [
+    slideInFromLeft, slideInFromRight
+  ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnChanges {
   selected = 'Relevant'
-  menu = ['Relevant', 'New']
+  menu = ['Relevant', 'New', 'Favourites', 'MyPost']
   @Input() authenticated: boolean = false;
 
   constructor(private service: StateService, private authService: AuthService) {
 
+  }
+
+  ngOnChanges(){ 
   }
 
   addArticle() {
