e86bbeeb9d73e8427dd42716526c78a5fb7b07cb
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-08-10 19:53:07 +0800

    Configured  simple cron job to docker swarm

---
+++ cron/update-actions.js
@@ -0,0 +1,8 @@
+const { port, environment } = require("./config").getEnv();
+console.log(
+  `Hi Enjoyable Programming this job with python runs each minute - ${new Date().toISOString()}`,
+);
+console.log(
+  "You can make a request, start a server, run a DB query or any job you want: ",
+  environment,
+);
