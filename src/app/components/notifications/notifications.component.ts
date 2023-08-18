680222d4fc95ac265e8f767e5e5bd47cc8bfa907
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-08-18 21:03:01 +0800

    Updating notification interface

--- src/app/components/notifications/notifications.component.ts
+++ src/app/components/notifications/notifications.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NotificationType } from 'src/app/core/interfaces/notification';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
 })
-export class NotificationsComponent {}
+export class NotificationsComponent {
+  notificationMessages: { [TYPE in NotificationType]: string } = {
+    emergency: 'An emergency was reported in your area:',
+    viral: 'This article is garnering attention in your area:',
+    interest: 'You might be interested in this one:',
+  };
+}
--- src/app/core/interfaces/notification.ts
+++ src/app/core/interfaces/notification.ts
@@ -1,9 +1,11 @@
 import { Article } from './article';
 import { IUser } from './auth';
 
+export type NotificationType = 'emergency' | 'viral' | 'interest';
+
 export interface Notification {
   _id: String;
-  type?: String;
+  type?: NotificationType;
   article: Article;
   users: Array<IUser>;
   time: Date;
