4dd9f4bd7a1b8a520de40971f98800f5bc57dd72
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-09-20 02:53:44 +0800

    minor cleanup

--- app-backend/src/clients/redis.client.ts
+++ app-backend/src/clients/redis.client.ts
@@ -4,31 +4,28 @@ dotenv.config();
 
 const RedisClient = createClient({
   url: process.env.REDIS_URL,
-});
-
-RedisClient.connect()
-  .then((res: any) => console.log(`Connection Succesful to RedisClient`))
-  .catch((err: any) => console.log(`Error in DB connection ${err}`));
-
-export default RedisClient;
+}); 
 
 export const RedisPublisher = RedisClient.duplicate();
 export const RedisSubscriber = RedisClient.duplicate();
 
-RedisPublisher.connect()
-  .then((res: any) => console.log(`Connection Succesful to RedisPublisher`))
+const clients: Record<string, typeof RedisClient> = {
+  'RedisClient': RedisClient,
+  'RedisPublisher': RedisPublisher,
+  'RedisSubscriber': RedisSubscriber
+}
+
+Object.keys(clients).forEach(name => {
+  clients[name].connect()
+  .then((res: any) => console.log(`Connection Succesful to ${name}`))
   .catch((err: any) => console.log(`Error in DB connection ${err}`));
 
-RedisSubscriber.connect()
-  .then((res: any) => console.log(`Connection Succesful to RedisSubscriber`))
-  .catch((err: any) => console.log(`Error in DB connection ${err}`));
+  clients[name].on("error", function (error) {
+    console.error(error);
+  });
+})
+ 
 
-RedisClient.on("error", function (error) {
-  console.error(error);
-});
-RedisSubscriber.on("error", function (error) {
-  console.error(error);
-});
-RedisPublisher.on("error", function (error) {
-  console.error(error);
-});
+export default RedisClient;
+
+
