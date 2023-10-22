4ac3b9ce973f9bcd29bf258ae1160dd324d77eba
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-10-23 04:06:58 +0800

    Update prod env on frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ const base_api = 'backend.dubstep-warrior.com/app/mapnews';
 export const environment = {
   production: true,
   endpoint_mapnews_backend_api: `https://${base_api}`,
-  ws_endpoint_mapnews_backend_api: `ws://${base_api}/ws`,
+  ws_endpoint_mapnews_backend_api: `wss://${base_api}/ws`,
 };
